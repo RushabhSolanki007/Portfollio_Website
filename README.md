3378906096b1ebfda45c15f8e9ef790d184651c5
RushabhSolanki007 <solankirushabh3593@gmail.com>
2024-06-19 21:11:14 +0530

    Initial commit

---
+++ README.md
@@ -0,0 +1,4 @@
+![Preview Img](files/1.png)
+![Preview Img](files/2.png)
+![Preview Img](files/3.png)
+![Preview Img](files/4.png)
---
+++ index.php
@@ -0,0 +1,307 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8"> 
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Sowndhar</title>
+    <link rel="icon" href="https://i.ibb.co/zRsTj3p/Frame-1-37.png">
+
+
+    <link rel="stylesheet" href="styles.css">
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.4.0/css/all.min.css" class="stylesheet">
+</head> 
+
+<body>
+
+    <!-------------------------- Header Area -------------------------->
+    <header class="header-area">
+        <div class="container">
+            <div class="header">
+               
+                <a href="" class="logo">
+                    <img src="files/signature.png" alt="" class="signature">
+                    <i class="fa fa-bolt"></i>
+                </a>
+                   
+                <ul class="navbar">
+                    <li><a href="#home">Home</a></li>
+                    <li><a href="#about">About</a></li>
+                    <li><a href="#education">Education</a></li>
+                    <li><a href="#projects">Projects</a></li>
+                    <li><a href="#contact">Contact</a></li>
+                </ul>
+                
+                <div class="menu_icon">
+                    <i class="fa fa-bars"></i>
+                </div>
+
+            </div>
+        </div> 
+    </header>
+ 
+    <!-------------------------- Home Page -------------------------->
+    <div class="FirstElement" id="home">
+        <div class="profile-photo">
+            <img src="files/myphoto.jpg" alt="profile picture">
+        </div>
+        <div class="profile-text">
+            <h5>Hi I'm </h5><br>
+            <h1>Rushabh</h1><br>
+            <p>Welcome to my portfolio website! I'm a passionate and aspiring web engineer with a strong desire to create innovative solutions and push the boundaries of technology.With a deep love for coding and problem-solving, I am constantly seeking opportunities to learn and grow in this ever-evolving field</p>
+            
+            <div class="btn-group">
+                <a href="files/resume.pdf" class="btn   active">Download CV</a>
+                <a href="mailto:solankirushabh3593@gmail.com" class="btn">Contact</a>
+            </div>
+
+            <div class="social">
+                <a href="mailto:solankirushabh3593@gmail.com"><i class="fa fa-envelope"></i></a>
+                <a href="https://github.com/RushabhSolanki007"><i class="fa-brands fa-github"></i></a>
+                <a href="https://www.linkedin.com/in/rushabh-solanki-263679243/"><i class="fa-brands fa-linkedin"></i></a>
+            </div>
+        </div>
+    </div>
+
+        <!-------------------------- About Section -------------------------->
+    <section class="about-area" id="about">
+        <div class="container">
+            <div class="about">
+                
+                <div class="about-content">
+                    <h4>About Me</h4>
+                    <ul>
+                      <li>I am currently pursuing my final year Bachelor's Degree in computer Engineering.I have built a solid foundation in frontend website devlopment, particularly in areas such as HTML, CSS, JAVASCRIPT and MYSQL.</li>
+                      <li> My expertise in these areas allows me to approach projects with confidence and deliver high-quality results.I have a strong belief in the value of continuous learning and staying adaptable in the ever-evolving world of software engineering.</li>
+                      <li> I actively seek out opportunities to expand my knowledge and skills, embracing new technologies and approaches.Feel free to explore my work and get in touch if you have any questions or opportunities to collaborate. </li>
+                    </ul>
+                </div>
+                                  
+                <div class="about-skills">
+                    <ul>
+                        <li>Name: Rushabh Girishbhai Solanki</li>
+                        <li>Age: 20</li>     
+                        <li>From: India</li>
+                        <li>Email: solankirushabh3593@gmail.com</li>
+                        <li>Availabiltiy: Fulltime,Remote</li>
+                        <li>Experience: Freser</li>
+                    </ul>
+                </div>
+            </div>
+        </div>
+    </section>
+
+
+        <!-------------- Education & Internship -------------->
+    <section class="education-content" id="education">
+        <div class="container">
+            <div class="row">
+                <div class="education ">
+                    <h3 class="title">Education</h3>
+                    <div class="row">
+                        <div class="timeline-box">
+                            <div class="timeline">
+
+                                <!-- Timeline-item -->
+                                <div class="timeline-item">
+                                    <div class="circle-dot"></div>
+                                    <h3 class="timeline-title">
+                                        B.E C.E.-75%
+                                    </h3>
+                                    <h4 class="timeline-title">Government Engineering Collge, Modasa</h4>
+                                    <h4 class="timeline-title">
+                                        <i class="fa fa-calendar"></i> 2022-2025(Pursuing)
+                                    </h4>
+                                </div> 
+                                <!-- Timeline-item -->
+                                <div class="timeline-item">
+                                    <div class="circle-dot"></div>
+                                    <h3 class="timeline-title">Diploma in C.E.-73%</h3>
+                                    <h4 class="timeline-title">C.U.Shah Government Polytechnic, Surendranagar</h4>
+                                    <h4 class="timeline-title">
+                                        <i class="fa fa-calendar"></i> 2019-2022
+                                    </h4>
+                                </div>
+                                <!-- Timeline-item -->
+                                <div class="timeline-item">
+                                    <div class="circle-dot"></div>
+                                    <h3 class="timeline-title">
+                                        SSC-72%(P.R-89.98)
+                                    </h3>
+                                    <h4 class="timeline-title"> Shree Sanskar Tirth Primary School</h4>
+                                    <h4 class="timeline-title">
+                                        <i class="fa fa-calendar"></i> 2017-2018
+                                    </h4>
+                                </div>
+
+                            </div>
+                        </div>
+                    </div>
+                </div>
+                <!--<div class="internship">
+                    <h3 class="title">Internship</h3>
+                    <div class="row">
+                        
+                        <div class="timeline-box">
+                            <div class="timeline">-->
+                                
+                                <!-- Timeline-item 
+                                <div class="timeline-item">
+                                    
+                                    <div class="circle-dot"></div>
+                                    
+                                    <h3 class="timeline-title">
+                                        ANDROID APPLICATION DEVELOPMENT
+                                    </h3>
+                                    <h4 class="timeline-title">FLARIAL TECHNOLOGY</h4>
+                                    <h4 class="timeline-title">
+                                        <i class="fa fa-calendar"></i> JUNE-2022
+                                    </h4>
+                                    <li class="timeline-text">Proficient in Android app development, with expertise in Java and building basic applications. Experienced in creating intuitive design and layout files. Completed a two-week internship, gaining practical skills and knowledge in the field.
+                                    </li>
+                                </div> -->
+                                
+                                <!-- Timeline-item 
+                                <div class="timeline-item">
+                                    <div class="circle-dot"></div>
+                                    <h3 class="timeline-title">PCB LAYOUT DESIGN AND FABRICATION</h3>
+                                    <h4 class="timeline-title">PIONEER CIRCUITS</h4>
+                                    <h4 class="timeline-title">
+                                        <i class="fa fa-calendar"></i> DEC-2021
+                                    </h4>
+                                    <li class="timeline-text">Experienced in PCB design and fabrication with a focus on Altium Designer software. Proficient in designing high-quality PCB boards. Two-week internship in the field, gaining hands-on experience and enhancing skills.</li>
+                                </div>
+                            -->
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </section>
+
+
+        <!-------------------------- Projects -------------------------->
+    <section class="project-content" id="projects">
+        <div class="container">
+            <div class="project-title">
+                <h4>My Projects</h4>
+                <p>Discover my projects, where creativity meets innovation</p>
+            </div>
+            <div class="projects">
+                <!-- 1st Project -->
+                <div class="project">
+                    <i class="fa fa-coffee"></i>
+                    <h4>ONE STORE (E-commerce site):</h4>
+                    <p> This responsive website has an online and offline shopping cart with an admin and seller panel. </p>
+                </div>
+                 <!-- 2nd Project -->
+                 <div class="project">
+                    <i class="fa fa-car"></i>
+                    <h4>Table Reservation System</h4>
+                    <p>This responsive website has an online table booking option in any restorer and also has an
+                        admin panel that can manage users</p>
+                </div>
+                <!-- 3rd Project -->
+                <div class="project">
+                    <i class="fa fa-desktop"></i>
+                    <h4>Portfolio Website</h4>
+                    <p>Created a portfolio website using HTML, CSS, and JavaScript, It serves as a platform to introduce myself, share my projects, and provide information about my  education and internship.</p>
+                </div>
+                
+            </div>
+        </div>
+    </section>
+
+        <!-------------------------- Contact Me -------------------------->
+
+    <section class="contact-content" id="contact">
+        <div class="container">
+           
+            <div class="contact-title">
+                <h4>Contact Me</h4>
+                <p>Get In Touch</p>
+            </div>
+            <div class="contact">
+                
+                <form name="submitToGoogleSheet " method ="POST">
+                    <input type="text" name="NAME" placeholder="Name" required>
+                    <input type="email" name="EMAIL" placeholder="Email" required>
+                    <input type="text" name="SUBJECT" placeholder="Subject">
+                    <textarea name="MESSAGE" placeholder="Message "name="MESSAGE"></textarea>
+                    <input type="submit" value="Send Message" class="submit" name="send">
+                    </div>
+                </form>
+</form>
+                <span id="msg"></span>
+            </div>
+        </div>
+    </section>
+    <?php
+
+    //Import PHPMailer classes into the global namespace
+//These must be at the top of your script, not inside a function
+use PHPMailer\PHPMailer\PHPMailer;
+use PHPMailer\PHPMailer\SMTP;
+use PHPMailer\PHPMailer\Exception;
+
+    if(isset($_POST['send']))
+    {
+        $name = $_POST['NAME'];
+        $email = $_POST['EMAIL'];
+        $subject = $_POST['SUBJECT'];
+        $message = $_POST['MESSAGE'];
+
+//Load Composer's autoloader
+require 'PHPMailer/Exception.php';
+require 'PHPMailer/PHPMailer.php';
+require 'PHPMailer/SMTP.php';
+
+//Create an instance; passing `true` enables exceptions
+$mail = new PHPMailer(true);
+
+try {
+    //Server settings
+    $mail->isSMTP();                                            //Send using SMTP
+    $mail->Host       = 'smtp.gmail.com';                     //Set the SMTP server to send through
+    $mail->SMTPAuth   = true;                                   //Enable SMTP authentication
+    $mail->Username   = 'solankirushabh3593@gmail.com';                     //SMTP username
+    $mail->Password   = 'hmjpdzmoesflmcnl';                               //SMTP password
+    $mail->SMTPSecure = PHPMailer::ENCRYPTION_SMTPS;            //Enable implicit TLS encryption
+    $mail->Port       = 465;                                    //TCP port to connect to; use 587 if you have set `SMTPSecure = PHPMailer::ENCRYPTION_STARTTLS`
+
+    //Recipients
+    $mail->setFrom('solankirushabh3593@gmail.com', 'contect form');
+    $mail->addAddress('rushabhsolanki2009@gmail.com', 'my id');     //Add a recipient
+    
+
+    //Content
+    $mail->isHTML(true);                                  //Set email format to HTML
+    $mail->Subject = 'content form';
+    $mail->Body    = "Sender Name - $name <br> Sender Email - $email <br> Subject - $subject <br> Message - $message ";
+
+    $mail->send();
+    echo 'Message has been sent';
+} catch (Exception $e) {
+    echo "Message could not be sent. Mailer Error: {$mail->ErrorInfo}";
+}
+
+        
+
+    }
+    ?>
+        
+    <!-------------------------- JS -------------------------->
+    <script src="https://code.jquery.com/jquery-3.7.0.min.js" integrity="sha256-2Pmvv0kuTBOenSvLm6bvfBSSHrUJ+3A7x6P5Ebd07/g=" crossorigin="anonymous"></script>
+    <script src="https://cdn.jsdelivr.net/npm/typed.js@2.0.12"></script>
+    <script src="https://unpkg.com/scrollreveal"></script>
+    <script src="script.js"></script>
+    
+    <!-------------------------- Footer section -------------------------->
+   <footer class="footer">
+    <small class="message">Thanks for visiting and happy coding!</small>
+    <small class="copyright">&copy; Copyright 2024. All rights reserved.</small>
+   </footer>
+
+</body>
+</html>
